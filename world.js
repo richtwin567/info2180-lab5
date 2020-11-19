c0abfbbe85ccead59493614b2a434d6cf6e431dc
richtwin567 <anakai.richards@gmail.com>
2020-11-19 01:33:24 -0500

    Exercise 3 complete

---
+++ world.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+	var headerimg = document.createElement("img");
+	headerimg.src = "undraw_connected_world_wuay.svg";
+	headerimg.style.objectFit = "contain";
+	headerimg.style.height = "7em";
+	var head = document.getElementsByTagName("header");
+	head.item(0).prepend(headerimg);
+
+	var cntrySearchBtn = document.getElementById("lookup");
+	cntrySearchBtn.addEventListener("click", (e) => {
+		var input = document.getElementById("country");
+		var query = input.value;
+		var results = document.getElementById("result");
+		fetch("http://localhost/info2180-lab5/world.php?country=" + query)
+			.then((res) => res.text())
+			.then((data) => 
+				results.innerHTML = data
+			);
+	});
+});
--- world.php
+++ world.php
@@ -3,13 +3,16 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
+if (array_key_exists("country", $_GET)) {
+  $country = $_GET["country"];
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+}
 ?>
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
